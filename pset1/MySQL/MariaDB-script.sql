b238057086e11df80f6cd82726030cdd87ef9e5a
Guilherme Tozzi Mafra <80405986+GuilhermeTozziMafra@users.noreply.github.com>
2022-04-23 11:53:44 -0400

    Update MariaDB-script.sql

--- pset1/MySQL/MariaDB-script.sql
+++ pset1/MySQL/MariaDB-script.sql
@@ -32,6 +32,73 @@ ALTER TABLE funcionario MODIFY COLUMN salario DECIMAL(10, 2) COMMENT 'Salário d
 
 ALTER TABLE funcionario MODIFY COLUMN cpf_supervisor CHAR(11) COMMENT 'CPF do supervisor. Será uma FK para a própria tabela (um auto-relacionamento).';
 
+/* INSERÇÃO DOS DADOS */
+/*Preenchendo a tabela de modo hierarquica*/
+INSERT INTO funcionario VALUES
+(88866555576, 'Jorge', 'E', 'Brito', '1937-11-10', 'Rua do Horto, 35, São Paulo, SP', 'M', 55000, NULL, 1), 
+-- Jorge é inserido primeiro, pois é o "supervisor dos supervisores", logo seu CPF vai ser necessário para os outros funcionários serem cadastrados
+(33344555587, 'Fernando', 'T', 'Wong', '1955-12-08', 'Rua da Lapa, 34, São Paulo, SP', 'M', 40000, 88866555576, 5),
+(98765432168, 'Jennifer', 'S', 'Souza', '1941-06-20', 'Av. Arthur de Lima, 54, Santo André, SP', 'F', 43000, 88866555576, 4), 
+(12345678966, 'João', 'B', 'Silva', '1965-01-09', 'Rua das Flores, 751, São Paulo, SP', 'M', 30000, 33344555587, 5), 
+(66688444476, 'Ronaldo', 'K', 'Lima', '1962-09-15', 'Rua Rebouças, 65, Piracicaba, SP', 'M', 38000, 33344555587, 5), 
+(45345345376, 'Joice', 'A', 'Leite', '1972-07-31', 'Av. Lucas Obes, 74, São Paulo, SP', 'F', 25000, 33344555587, 5), 
+(99988777767, 'Alice', 'J', 'Zelaya', '1968-01-19', 'Rua Souza Lima, 35, Curitiba, PR', 'F', 25000, 98765432168, 4), 
+(98798798733, 'André', 'V', 'Pereira', '1969-03-29', 'Rua Timbira, 35, São Paulo, SP', 'M', 25000, 98765432168, 4); 
+
+/*Preenchendo a tabela departamento*/
+INSERT INTO departamento VALUES
+(1, 'Matriz', 88866555576, '1981-06-19'), 
+(5, 'Pesqusia', 33344555587, '1988-05-22'), 
+(4, 'Administração', 98765432168, '1995-01-01'); 
+
+/*Preenchendo a tabela localizacoes_departamento*/
+INSERT INTO localizacoes_departamento VALUES
+(1, 'São Paulo'),
+(4, 'Mauá'), 
+(5, 'Santo André'), 
+(5, 'Itu'), 
+(5, 'São Paulo'); 
+
+/*Preenchendo a tabela projeto*/
+INSERT INTO projeto VALUES
+(1, 'ProdutoX', 'Santo André', 5), 
+(2, 'ProdutoY', 'Itu', 5), 
+(3, 'ProdutoZ', 'São Paulo', 5), 
+(10, 'Informatização', 'Mauá', 4), 
+(20, 'Reorganização', 'São Paulo', 1), 
+(30, 'Novosbenefícios', 'Mauá', 4); 
+
+/*Preenchendo a tabela dependente*/
+INSERT INTO dependente VALUES
+(33344555587, 'Alicia', 'F', '1986-04-05', 'Filha'), 
+(33344555587, 'Tiago', 'M', '1983-10-25', 'Filho'), 
+(33344555587, 'Janaína', 'F', '1958-05-03', 'Esposa'), 
+(98765432168, 'Antonio', 'M', '1942-02-28', 'Marido'), 
+(12345678966, 'Michael', 'M', '1988-01-04', 'Filho'), 
+(12345678966, 'Alicia', 'F', '1988-12-30', 'Filha'), 
+(12345678966, 'Elizabeth', 'F', '1967-05-05', 'Esposa'); 
+
+/*Preenchendo a tabela trabalha_em*/
+INSERT INTO trabalha_em VALUES
+(12345678966, 1, 32.5), 
+(12345678966, 2, 7.5), 
+(66688444476, 3, 40), 
+(45345345376, 1, 20), 
+(45345345376, 2, 20),
+(33344555587, 2, 10),
+(33344555587, 3, 10), 
+(33344555587, 10, 10), 
+(33344555587, 20, 10), 
+(99988777767, 30, 30), 
+(99988777767, 10, 10), 
+(98798798733, 10, 35), 
+(98798798733, 30, 5), 
+(98765432168, 30, 20), 
+(98765432168, 20, 15), 
+(88866555576, 20, NULL); 
+
+/* TODOS OS DADOS FORAM Preenchidos COM SUCESSO! */
+
 ALTER TABLE funcionario MODIFY COLUMN numero_departamento INTEGER COMMENT 'Número do departamento do funcionário.';
 
 
