9123a38fe1294695167dcd0af55034d335ef5b1d
Guilherme Tozzi Mafra <80405986+GuilhermeTozziMafra@users.noreply.github.com>
2022-05-07 17:28:04 -0400

    Update MySQLscript.sql

--- pset2/Questões pset2/MySQLscript.sql
+++ pset2/Questões pset2/MySQLscript.sql
@@ -1,1 +1,10 @@
+--- Questão 1 ---
+select numero_departamento, round (avg(salario),2) as média_salarial from funcionario group by numero_departamento;
+
+--- Questão 2 ---
+select sexo, round (avg(salario),2) as média_salarial from funcionario  group by sexo;
+
+
+--- Questão 10 ---
+select numero_departamento, round (avg(salario),2) as média_salarial from funcionario group by numero_departamento;
 
