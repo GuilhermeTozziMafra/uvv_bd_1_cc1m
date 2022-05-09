e6347029185d6d2fc46db9bbc5915aaa51d2f0fa
Guilherme Tozzi Mafra <80405986+GuilhermeTozziMafra@users.noreply.github.com>
2022-05-09 17:29:15 -0400

    Update MySQLscript.sql

--- pset2/Questões pset2/MySQLscript.sql
+++ pset2/Questões pset2/MySQLscript.sql
@@ -8,8 +8,14 @@ select sexo, round (avg(salario),2) as média_salarial from funcionario  group b
 select concat(primeiro_nome, nome_meio, ultimo_nome) as nome_completo, year(current_timestamp())-year(data_nascimento) as idade, salario from funcionario where salario * 1.20 < 35000 or salario * 1.15 >= 35000;
 
 --- Questão 6 ---
-select distinct concat(primeiro_nome, nome_meio, ultimo_nome) as nome_funcionario, numero_departamento, concat(d.nome_dependente, f.nome_meio, f.ultimo_nome) as nome_dependente, year(current_timestamp())-year(d.data_nascimento) as idade_dependente, if(d.sexo = "M","Masculino","Feminino") as sexo from funcionario f, dependente
+select distinct concat(primeiro_nome, nome_meio, ultimo_nome) as nome_funcionario, numero_departamento, concat(d.nome_dependente, f.nome_meio, f.ultimo_nome) as nome_dependente, year(current_timestamp())-year(d.data_nascimento) as idade_dependente, if(d.sexo = "M","Masculino","Feminino") as sexo
+from funcionario f, dependente
 inner join dependente d where f.cpf = d.cpf_funcionario;
+
+--- Questão 8 ---
+select distinct numero_departamento, trabalha_em.numero_projeto, concat(f.primeiro_nome, f.nome_meio, f.ultimo_nome) as nome_funcionario,horas
+from funcionario as f
+inner join trabalha_em on f.cpf = trabalha_em.cpf_funcionario order by numero_departamento;
 
 --- Questão 10 ---
 select numero_departamento, round (avg(salario),2) as média_salarial from funcionario group by numero_departamento;
